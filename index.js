922bae4416e1a3f29081440e330748a6b8d3c2b7
Zohan Mimo <zohan@mimo.bot>
2026-07-15 18:28:59 +0300

    Исправлен fs is not defined - добавлен require('fs') для server-config.json

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 // Настройки сервера (сохраняются между перезапусками)
+const fs = require('fs');
 const path = require('path');
 const CONFIG_FILE = path.join(__dirname, 'server-config.json');
 function loadConfig() {
